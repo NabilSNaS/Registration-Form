c5555b3516f3c3bde143c4087b5da02484e996e9
Hafija Aktar Nabila <35609343+NabilSNaS@users.noreply.github.com>
2022-07-08 18:32:00 +0600

    DB Name: sinup

---
+++ dbo.reg.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[reg]
+(
+[fname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[lname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[email] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[gender] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[address] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[phone] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[pass] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
